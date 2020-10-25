955f955c64b8ac99403693b773ecb8f83dc12560
布衣 <170001330@qq.com>
2020-10-26 03:56:23 +0800

    Update cm.js

--- js/cm.js
+++ js/cm.js
@@ -5,7 +5,11 @@ let _w = window.innerWidth||document.documentElement.clientWidth||document.body.
 let _info = document.getElementById('links');
 
 //判断浏览器大小
-_w > 400 ? _info.style.display = 'block' : _info.style.display = 'none';
+if(_w > 400){
+_info.style.display = 'block';
+}else{
+_info.style.display = 'none';
+}
 
 //请保留版权
 if (window.console && window.console.log) {
