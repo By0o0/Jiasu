ac91ea5f9b45b1a91e2833dfad3681b4a3b20efc
布衣 <170001330@qq.com>
2020-10-26 04:12:15 +0800

    Update cm.js

--- js/cm.js
+++ js/cm.js
@@ -1,14 +1,14 @@
 //获取浏览器宽度
-let _w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+let w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
 
 //获得link id
-let _info = document.getElementById('links');
+let info = document.getElementById('links');
 
 //判断浏览器大小
-if(_w > 400){
-_info.style.display = 'block';
+if(w > 400){
+info.style.display = 'block';
 }else{
-_info.style.display = 'none';
+info.style.display = 'none';
 }
 
 //请保留版权
