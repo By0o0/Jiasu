87d3aeab897e50ba28cd2e33ff90965ef394bf16
布衣 <170001330@qq.com>
2020-10-26 03:44:55 +0800

    Delete cm.js

--- js/cm.js
+++
@@ -1,9 +0,0 @@
-
-//获取浏览器宽度
-let w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
-
-//获得link id
-let info = document.getElementById('links');
-
-//判断浏览器大小
-w > 400 ? info.style.display = 'block' : info.style.display = 'none';
