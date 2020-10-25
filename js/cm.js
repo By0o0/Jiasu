20e3d69127bf04e1611584afc799c47d454194dc
布衣 <170001330@qq.com>
2020-10-26 03:42:00 +0800

    Update cm.js

--- js/cm.js
+++ js/cm.js
@@ -1,9 +1,9 @@
 
 //获取浏览器宽度
-let width = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+let w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
 
 //获得link id
-let _info = document.getElementById('links');
+let info = document.getElementById('links');
 
 //判断浏览器大小
-width > 400 ? _info.style.display = 'block' : _info.style.display = 'none';
+w > 400 ? info.style.display = 'block' : info.style.display = 'none';
