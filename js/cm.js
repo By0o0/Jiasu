3cdf7b7f76bfd92704a6778c5aeb43da2f00353d
布衣 <170001330@qq.com>
2020-10-26 03:54:14 +0800

    Update cm.js

--- js/cm.js
+++ js/cm.js
@@ -1,11 +1,11 @@
 //获取浏览器宽度
-let w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+let _w = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
 
 //获得link id
-let info = document.getElementById('links');
+let _info = document.getElementById('links');
 
 //判断浏览器大小
-w > 400 ? info.style.display = 'block' : info.style.display = 'none';
+_w > 400 ? _info.style.display = 'block' : _info.style.display = 'none';
 
 //请保留版权
 if (window.console && window.console.log) {
